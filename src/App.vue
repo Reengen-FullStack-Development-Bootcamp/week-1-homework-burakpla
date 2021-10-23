c61cb9b8bad3b1e6929ba3ec0cc9e10b3072e824
Burak Pala <74012242+burakpla@users.noreply.github.com>
2021-10-23 15:09:22 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Header />
+    <div class="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-8"><Shop /></div>
+          <div class="col-4"><Cart /></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Header from "./components/Header";
+import Shop from "./components/Shop";
+import Cart from "./components/Cart";
+export default {
+  name: "App",
+  components: {
+    Shop,
+    Header,
+    Cart,
+  },
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color:black;
+   border-block-color:black ;
+}
+</style>
---
+++ src/components/Shop.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="row">
+    <div
+      class="card"
+      style="width: 15rem;"
+      v-for="item in products"
+      :key="item.id"
+    >
+      <img :src="item.imgUrl" class="card-img-top" :alt="item.title" />
+
+      <div class="card-body">
+        <p class="card-title">{{ item.title }}</p>
+                <p class="card-info">{{ item.info }}</p>
+                                <p class="card-size">{{ item.size }}</p>
+
+
+        <p class="card-text">$ {{ item.price.toLocaleString() }}</p>
+        <button class="btn btn-secondary bg-gray" @click="selectSize(size)">
+          {{size="37"}}
+        </button>
+        <button class="btn btn-secondary" @click="selectSize(size)">
+          {{size="38"}}
+        </button>
+        <button class="btn btn-secondary" @click="selectSize(size)">
+          {{size="39"}}
+        </button>
+        <button class="btn btn-secondary" @click="selectSize(size)">
+          {{size="40"}}
+        </button>
+         <button class="btn btn-secondary" @click="selectSize(size)">
+          {{size="41"}}
+        </button>
+         <button class="btn btn-secondary" @click="selectSize(size)">
+          {{size="42"}}
+        </button>
+         
+        <hr>
+        <button class="btn btn-secondary bg-black" @click="addItemToCart(item)">
+          Add To Cart
+        </button>
+        
+        
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+export default {
+  name: "Shop",
+  mounted() {
+    this.getProducts();
+  },
+  computed: {
+    ...mapGetters(["products"]),
+  },
+  methods: {
+    ...mapActions(["addItemToCart", "getProducts"]),
+    ...mapActions(["selectSize"])
+  },
+};
+</script>
+<style scoped>
+.card {
+  padding: 20px
+  ;
+  border: 100px;
+  background-color: rgb(252, 250, 250);
+
+}
+.btn{
+  background-color:coral;
+}
+</style>
